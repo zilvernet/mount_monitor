543b8b4f370d65f309f5946b7fb1baaeb11f322c
zilvernet <75147745+zilvernet@users.noreply.github.com>
2023-03-31 14:23:08 -0700

    Update mount_docker.py

--- mount_docker.py
+++ mount_docker.py
@@ -1,13 +1,16 @@
 import configparser
 import subprocess
+import os
+
+script_path = os.path.dirname(os.path.abspath(__file__))
+config_path = os.path.join(script_path, 'mountmonitor-config.ini')
 
 # Read the container names from the INI file
 config = configparser.ConfigParser()
-config.read('mountmonitor-config.ini')
+config.read(f'{config_path}')
 containers = config.items('containers')
 
 # Loop through the containers and restart them
 for index, container_name in containers:
-    print(f"Restarting container {container_name}...")
-    subprocess.run(['docker', 'restart', container_name], check=True)
+    subprocess.run(['docker', 'restart', container_name], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     print(f"Container {container_name} restarted.")
