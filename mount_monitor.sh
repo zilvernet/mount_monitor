6bc915332457e9d0b2490d58c288067e710c7346
zilvernet <75147745+zilvernet@users.noreply.github.com>
2023-03-31 19:49:33 -0700

    Update mount_monitor.sh

--- mount_monitor.sh
+++ mount_monitor.sh
@@ -2,30 +2,50 @@
 
 # Path to the INI file
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
-INI_FILE="$SCRIPT_DIR/mount-status.ini"
+INI_FILE="$SCRIPT_DIR/mount-status.txt"
 CONFIG_FILE="$SCRIPT_DIR/mountmonitor-config.ini"
 
 SLACK_WEBHOOK_URL=$(awk -F'=' '/^\[Slack\]/{f=1} f==1&&$1~/^slack_hook_url/{print $2;exit}' $CONFIG_FILE)
 SLACK_CHANNEL=$(awk -F'=' '/^\[Slack\]/{f=1} f==1&&$1~/^slack_channel/{print $2;exit}' $CONFIG_FILE)
 
 # Read the list of mounts to check from the config file
-MOUNTS=""
+MOUNTS_IN_CONFIG=""
 if [ -f "$CONFIG_FILE" ]; then
     section_start=$(awk '/^\[mounts\]/{print NR+1; exit 0;}' "$CONFIG_FILE")
-    section_end=$(awk "/^\[[^m][^o][^u][^n][^t][^s]/ {print NR; exit 0;}" "$CONFIG_FILE")
+    section_end=$(awk '/^\[containers\]/{print NR-1; exit 0;}' "$CONFIG_FILE")
+    #echo "section_start:$section_start section_end:$section_end"
     if [ -z "$section_end" ]; then
         section_end="$(wc -l < "$CONFIG_FILE")"
     fi
-    MOUNTS=$(awk -F= -v start=$section_start -v end=$section_end 'NR>=start && NR<end{printf "%s ", $2}' "$CONFIG_FILE" | tr -d '\r\n') > /dev/null 2>&1
-    if [ -z "$MOUNTS" ]; then
+
+    for ((i=$section_start;i<$section_end;i++)); do
+        line=$(sed -n "${i}p" "$CONFIG_FILE")  # Get the line from config file
+        mount=$(echo "$line" | cut -d= -f2)
+        MOUNTS_IN_CONFIG+="${mount} "  # Concatenate the mount to the existing string with a space separator
+    done
+    MOUNTS_IN_CONFIG=${MOUNTS_IN_CONFIG% }  # Remove trailing space
+    if [ -z "$MOUNTS_IN_CONFIG" ]; then
         echo "No mounts found in the [mounts] section of the config file"
         exit 1
     fi
+else
+    echo "No config file was found in: $CONFIG_FILE"
+    exit 1
 fi
 
-# Check if the number of mounts is different than the ones in the INI file
-MOUNTS_IN_INI=$(grep -c '^\s*/mnt/' "$INI_FILE")
-if [ "$MOUNTS_IN_INI" -ne $(echo "$MOUNTS" | wc -w) ]; then
+# Read the list of mounts to check from the status file
+MOUNTS_IN_STATUS=""
+if [ -f "$INI_FILE" ]; then
+    while read -r line; do
+        mount=$(echo "$line" | cut -d= -f1)
+        echo "mount:$mount"
+        MOUNTS_IN_STATUS+="${mount} "  # Concatenate the mount to the existing string with a space separator
+    done < "$INI_FILE"
+    MOUNTS_IN_STATUS=${MOUNTS_IN_STATUS% }  # Remove trailing space
+fi
+
+# Check if the number of mounts is different than the ones in the INI config file
+if [ "$MOUNTS_IN_STATUS" != "$MOUNTS_IN_CONFIG" ]; then
     echo "Number of mounts in the INI file is different than the ones in the config file, recreating the INI file"
     rm -f "$INI_FILE"
 fi
@@ -35,7 +55,7 @@ if [ ! -f "$INI_FILE" ]; then
     # Create the INI file with the initial mount status
     touch "$INI_FILE"
     chmod 644 "$INI_FILE"
-    for mount in $MOUNTS; do
+    for mount in $MOUNTS_IN_CONFIG; do
         echo "$mount=1" >> "$INI_FILE"
     done
 fi
@@ -43,15 +63,27 @@ fi
 declare -A MOUNT_STATUS
 if [ -f "$INI_FILE" ]; then
     while IFS='=' read -r key value; do
-        if [[ $key == /mnt/* ]]; then
-            MOUNT_STATUS["$key"]="$value"
-        fi
-    done < <(sort "$INI_FILE" | uniq)
+        MOUNT_STATUS["$key"]="$value"
+    done < "$INI_FILE"
 else
-    for mount in $MOUNTS; do
+    for mount in $MOUNT_IN_STATUS; do
         MOUNT_STATUS["$mount"]="1"
     done
 fi
+
+declare -A MOUNTS
+i=0
+for mount in $MOUNTS_IN_STATUS; do
+    MOUNTS[$i]=$mount
+    ((i++))
+done
+
+#for mount in "${!MOUNT_STATUS[@]}"; do
+#    echo "MOUNTS_STATUS $mount has status ${MOUNT_STATUS[$mount]}"
+#done
+#for mount in "${!MOUNTS[@]}"; do
+#    echo "MOUNTS $mount has mountpoint ${MOUNTS[$mount]}"
+#done
 
 # Check each mount and set MOUNT_DISCONNECTED to 1 if any mount is disconnected
 MOUNT_DISCONNECTED=0
@@ -69,6 +101,9 @@ for mount in $MOUNTS; do
         fi
     fi
 done
+
+echo "MOUNT_DISCONNECTED=$MOUNT_DISCONNECTED"
+read -p "press ENTER to continue"
 
 # If any mount is disconnected, reconnect the mounts with sudo mount -a
 if [ $MOUNT_DISCONNECTED -eq 1 ]; then
